b8e6dc7fac66ece1e2f1728013c0d2d7b3704513
bianca-tee <112151662+bianca-tee@users.noreply.github.com>
2022-09-10 14:20:19 +1000

    Max/min temp in forecast flipped, max changed to <strong>, padding added to forecast

--- script.js
+++ script.js
@@ -57,11 +57,11 @@ function displayForecast(response) {
         />
         <div class="weather-forecast-temperatures">
           <span class="weather-forecast-temperature-max"> ${Math.round(
-            forecastDay.temp.max
-          )}° </span>
-          <span class="weather-forecast-temperature-min"> ${Math.round(
             forecastDay.temp.min
-          )}° </span>
+          )}° </span>/
+          <strong class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.max
+          )}° </strong>
         </div>
       </div>
   `;
