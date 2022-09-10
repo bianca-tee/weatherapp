ce7fa425f74478c9fed32264da837485daa00057
bianca-tee <112151662+bianca-tee@users.noreply.github.com>
2022-09-10 11:46:45 +1000

    Added notes in JS to make it easier to follow and debug, for the timebeing
    Also changed apiURL for getForecast as previous one was not working, and noted redundancy in the function getCity (now hidden, likely to be removed in future)

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Determining the current time and date
+
 function formatDate(date) {
   let hours = date.getHours();
   if (hours < 10) {
@@ -18,10 +20,16 @@ function formatDate(date) {
     "Saturday",
   ];
   let currentDay = days[date.getDay()];
-  return `${currentDay} ${hours}:${minutes}`;
+  return `Last updated at ${currentDay} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+let currentTime = new Date();
+let dateTimeElement = document.querySelector("#current-date-time");
+dateTimeElement.innerHTML = formatDate(currentTime);
+
+//Displays the forecast with the coord data from below
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
   let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
   let forecastHTML = `<div class="row">`;
@@ -38,10 +46,14 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
-let currentTime = new Date();
-let dateTimeElement = document.querySelector("#current-date-time");
-dateTimeElement.innerHTML = formatDate(currentTime);
+//Function that uses coords in an apiURL that allows us to get forecast, then runs fn displayForecast
+function getForecast(coordinates) {
+  let apiKey = "a43564c91a6c605aeb564c9ed02e3858";
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 
+//Uses the lat and long to show the temp and city name, plus refers to function to get the forecast
 function showTempCity(response) {
   celsiusTemperature = response.data.main.temp;
   document.querySelector(`#current-city`).innerHTML = response.data.name;
@@ -66,8 +78,10 @@ function showTempCity(response) {
       "src",
       `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
     );
+  getForecast(response.data.coord);
 }
 
+//Gets the city from the search form then runs the API using city name to get weather data, then runs the function showTempCity
 function searchCity(event) {
   event.preventDefault();
   let city = document.querySelector("#city-input").value;
@@ -76,33 +90,41 @@ function searchCity(event) {
   axios.get(url).then(showTempCity);
 }
 
+//Selects and adds event listener to the city search form, then runs searchCity when submitted
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", searchCity);
 
-function getCity(response) {
-  let cityHere = response.data.locality;
-  let cityHeading = document.querySelector("#current-city");
-  cityHeading.innerHTML = cityHere;
-}
+//Gets the city name using the lat long data although seems to be redundant given the function showTempCity seems to also produce city name
+//function getCity(response) {
+//let cityHere = response.data.locality;
+//let cityHeading = document.querySelector("#current-city");
+//cityHeading.innerHTML = cityHere;
+//}
 
+//Accesses lat and long to do 2 things: get weather and get city based on lat and long
 function weatherHere(position) {
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
   let apiKey = "0fbc2c49d4b2c78e5faff6668811ca96";
   let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&lang=en&units=metric`;
   axios.get(weatherUrl).then(showTempCity);
-  let cityUrl = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lon}&localityLanguage=en`;
-  axios.get(cityUrl).then(getCity);
+  //let cityUrl = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lon}&localityLanguage=en`;
+  //axios.get(cityUrl).then(getCity);
 }
 
+//Gets the position then tells it to run the weatherHere function with the data
 function currentLocation(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(weatherHere);
 }
 
+//Selected location pin and added an event listener to run currentLocation function
 let locationPin = document.querySelector(`#location-pin`);
 locationPin.addEventListener("click", currentLocation);
 
+//Celsius and Fahrenheit switch
+
+//Function to run when fahrenheit switch clicked
 function convertFahrenheit(event) {
   event.preventDefault();
   let tempElement = document.querySelector("#current-temp");
@@ -110,18 +132,26 @@ function convertFahrenheit(event) {
   tempElement.innerHTML = `${Math.round(fahrenheitTemp)}°`;
 }
 
+//Function to run when celsius link clicked - conversion
 function convertCelsius(event) {
   event.preventDefault();
   let tempElement = document.querySelector("#current-temp");
   tempElement.innerHTML = `${Math.round(celsiusTemperature)}°`;
 }
 
+//Created global variable, which can be accessed from within function
+//Setting it initially as null, then making it the celsius temperature within the showTempCity function
+//Now it represents the accurate temperature, rather than just what is in that HTML element
+//Can be used for above calculation in the conversion
 let celsiusTemperature = null;
 
+//Added event listener to fahrenheit link when clicked
 document
   .querySelector(`#fahrenheit`)
   .addEventListener("click", convertFahrenheit);
 
+//Added event listener to celsius link when clicked
 document.querySelector("#celsius").addEventListener("click", convertCelsius);
 
-displayForecast();
+//Run a search of Sydney when page loads, as default
+searchCity("Sydney");
